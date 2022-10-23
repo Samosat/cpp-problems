d2348d9520c28869b80f7416203575e21069296b
unknown <samvelkhach33@gmail.com>
2022-10-23 17:09:40 +0400

    my first commit

---
+++ first_git_file.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+ int main(){
+    int w;
+    cin >> w;
+    if(w <= 2){
+        cout << "NO" << endl;
+        return 0;
+    }
+    if(w % 2){
+        cout << "NO" << endl;
+        return 0;
+    }
+    cout << "YES"
+    return 0;
+ }
